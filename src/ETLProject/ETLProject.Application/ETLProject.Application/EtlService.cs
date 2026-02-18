3ffcf945b34147fd74b51fa4be4c85fccccadedd
Fririz <Fririzriz@gmail.com>
2026-02-18 14:42:21 +0200

    add func timeconvert and normalizeFlag

--- src/ETLProject/ETLProject.Application/ETLProject.Application/EtlService.cs
+++ src/ETLProject/ETLProject.Application/ETLProject.Application/EtlService.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
 using ETLProject.Domain.Interfaces;
+using ETLProject.Domain.Models;
 
 namespace Application;
 
@@ -9,9 +10,29 @@ public class EtlService : IEtlService
 {
     private readonly ISqlRepository _repository;
     private readonly ICsvService _csvService;
+    private readonly TimeZoneInfo _estTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/New_York"); //because runs in linux container
     public EtlService(ISqlRepository repository, ICsvService csvService)
     {
         _repository = repository;
         _csvService = csvService;
+
+    }
+    //                         pickup    dropoff   passengers
+    private readonly HashSet<(DateTime, DateTime, int)> _uniqueKeys = new();
+    
+    private string NormalizeFlag(string? flag)
+    {
+        var cleaned = flag?.Trim();
+        return cleaned switch
+        {
+            "Y" => "Yes",
+            "N" => "No",
+            _ => "No"
+        };
+    }
+    
+    private DateTime ConvertToUtc(DateTime estTime)
+    {
+        return TimeZoneInfo.ConvertTimeToUtc(estTime, _estTimeZone);
     }
 }
