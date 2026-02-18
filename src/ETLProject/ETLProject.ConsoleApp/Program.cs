091d0dead6b047da201cf57059424c57e590b22c
Fririz <Fririzriz@gmail.com>
2026-02-18 16:32:05 +0200

    CLI implemented

--- src/ETLProject/ETLProject.ConsoleApp/Program.cs
+++ src/ETLProject/ETLProject.ConsoleApp/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using ETLProject.Domain.Interfaces;
+using ETLProject.Infrastructure;
+using Application;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection") 
+                               ?? throw new InvalidOperationException("ConnectionString not found.");
+
+        services.AddScoped<ISqlRepository>(_ => new SqlRepository(connectionString));
+        services.AddScoped<ICsvService, CsvService>();
+        services.AddScoped<IEtlService, EtlService>();
+    })
+    .Build();
+
+try
+{
+    using var scope = host.Services.CreateScope();
+    var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+    var etlService = scope.ServiceProvider.GetRequiredService<IEtlService>();
+
+    string input = config["FilePaths:InputCsv"] ?? "data/sample-cab-data.csv";
+    string output = config["FilePaths:DuplicatesCsv"] ?? "data/duplicates.csv";
+        
+    var outDir = Path.GetDirectoryName(output);
+    if (!string.IsNullOrEmpty(outDir)) Directory.CreateDirectory(outDir);
+
+    if (!File.Exists(input)) throw new FileNotFoundException($"File not found: {input}");
+
+    Console.WriteLine($"Starting Import");
+    
+    await etlService.RunImportAsync(input, output);
+
+    Console.WriteLine("Done");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
--- src/ETLProject/ETLProject.Infrastructure/SqlRepository.cs
+++ src/ETLProject/ETLProject.Infrastructure/SqlRepository.cs
@@ -1,8 +1,7 @@
-using ETLProject.Domain.Interfaces;
 using System.Data;
+using ETLProject.Domain.Interfaces;
 using ETLProject.Domain.Models;
 using Microsoft.Data.SqlClient;
-
 
 namespace ETLProject.Infrastructure;
 
@@ -18,15 +17,17 @@ public class SqlRepository : ISqlRepository
     public async Task BulkInsertTripsAsync(IEnumerable<DbModel> trips)
     {
         var table = new DataTable();
-        table.Columns.Add("PickupDatetime", typeof(DateTime));
-        table.Columns.Add("DropoffDatetime", typeof(DateTime));
-        table.Columns.Add("PassengerCount", typeof(int));
-        table.Columns.Add("TripDistance", typeof(double));
-        table.Columns.Add("StoreAndFwdFlag", typeof(string));
-        table.Columns.Add("PULocationId", typeof(int));
-        table.Columns.Add("DOLocationId", typeof(int));
-        table.Columns.Add("FareAmount", typeof(decimal));
-        table.Columns.Add("TipAmount", typeof(decimal));
+        
+
+        table.Columns.Add(nameof(DbModel.PickupDatetime), typeof(DateTime));
+        table.Columns.Add(nameof(DbModel.DropoffDatetime), typeof(DateTime));
+        table.Columns.Add(nameof(DbModel.PassengerCount), typeof(int));
+        table.Columns.Add(nameof(DbModel.TripDistance), typeof(double));
+        table.Columns.Add(nameof(DbModel.StoreAndFwdFlag), typeof(string));
+        table.Columns.Add(nameof(DbModel.PULocationId), typeof(int));
+        table.Columns.Add(nameof(DbModel.DOLocationId), typeof(int));
+        table.Columns.Add(nameof(DbModel.FareAmount), typeof(decimal));
+        table.Columns.Add(nameof(DbModel.TipAmount), typeof(decimal));
         
         foreach (var trip in trips)
         {
@@ -43,20 +44,24 @@ public class SqlRepository : ISqlRepository
             );
         }
 
-        using var bulkCopy = new SqlBulkCopy(_connectionString, SqlBulkCopyOptions.TableLock);
+
+        using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+        
+        using var bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.TableLock, null);
         bulkCopy.DestinationTableName = "Trips";
         bulkCopy.BatchSize = 10000;
         bulkCopy.BulkCopyTimeout = 600;
-
-        bulkCopy.ColumnMappings.Add("PickupDatetime", "tpep_pickup_datetime");
-        bulkCopy.ColumnMappings.Add("DropoffDatetime", "tpep_dropoff_datetime");
-        bulkCopy.ColumnMappings.Add("PassengerCount", "passenger_count");
-        bulkCopy.ColumnMappings.Add("TripDistance", "trip_distance");
-        bulkCopy.ColumnMappings.Add("StoreAndFwdFlag", "store_and_fwd_flag");
-        bulkCopy.ColumnMappings.Add("PULocationId", "PULocationID");
-        bulkCopy.ColumnMappings.Add("DOLocationId", "DOLocationID");
-        bulkCopy.ColumnMappings.Add("FareAmount", "fare_amount");
-        bulkCopy.ColumnMappings.Add("TipAmount", "tip_amount");
+        
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.PickupDatetime), "tpep_pickup_datetime");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.DropoffDatetime), "tpep_dropoff_datetime");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.PassengerCount), "passenger_count");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.TripDistance), "trip_distance");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.StoreAndFwdFlag), "store_and_fwd_flag");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.PULocationId), "PULocationID");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.DOLocationId), "DOLocationID");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.FareAmount), "fare_amount");
+        bulkCopy.ColumnMappings.Add(nameof(DbModel.TipAmount), "tip_amount");
         
         await bulkCopy.WriteToServerAsync(table);
     }
